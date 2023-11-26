681e6210759718a6c13d4aa18da8c7220ef9ee76
brandie8 <brandie120@gmail.com>
2023-11-26 18:54:03 +0300

    corrected a typo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function App() {
+function Homepage() {
   return (
     <div>
       {/* First Container */}
@@ -156,4 +156,4 @@ function App() {
   );
 }
 
-export default App;
+export default Homepage;
