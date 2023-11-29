b0f1cbb6799dcbfa98c6a4b1baaea7fe21acc0d2
brandie8 <brandie120@gmail.com>
2023-11-29 10:07:03 +0300

    The muzaddeplus page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-import ForgotPassword from './Components/ForgotPassword'; 
+import FAQ from './Components/FAQ';
+import Subscription from './Components/Subscription';
 
-const App = () => {
+const faqData = [
+  {
+    question: 'What is Muzadde Today?',
+    answer: 'Muzadde Today is a parent platform for advice on how to raise children.',
+  },
+  {
+    question: 'Is Muzadde Today a paid-only support community?',
+    answer: 'Non-members are also able to get a limited service from Muzadde Today',
+  },
+  {
+    question: 'How long will it take me to get feedback from a specialist?',
+    answer: 'You will get feedback from a specialist within 10 minutes',
+  },
+  //  We can add more FAQ items here
+];
+
+function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} /> 
-        <Route path="/*" element={<Login />} /> {/* Default route */}
+        <Route path="/" element={<div className="App">
+         
+          <FAQ data={faqData} />
+        </div>} />
+        <Route path="/subscription" element={<Subscription />} />
       </Routes>
     </Router>
   );
