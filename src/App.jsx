be5253fbd32d80a747bcb4fc6a9a2f952bff6155
brandie8 <brandie120@gmail.com>
2023-12-04 21:07:23 +0300

    added the Talks page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
+// App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Components/Login';
-import Signup from './Components/Signup';
-import ForgotPassword from './Components/ForgotPassword'; 
+import Talks from './Components/Talks';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} /> 
-        <Route path="/*" element={<Login />} /> {/* Default route */}
-      </Routes>
-    </Router>
+    <div className="App">
+      <Talks />
+    </div>
   );
 }
 
