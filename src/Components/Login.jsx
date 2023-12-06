7fef9d9d319227e5308221a39f613533b92cc46a
brandie8 <brandie120@gmail.com>
2023-12-06 17:30:53 +0300

    installed the auth0 login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
+
+  const { loginWithRedirect } = useAuth0(); // Use loginWithRedirect
 
   const handleRememberMeChange = () => {
     setRememberMe(!rememberMe);
@@ -21,9 +24,13 @@ function Login() {
     }
   };
 
+  const handleLoginWithAuth0 = () => {
+    // Redirect to Auth0 login page
+    loginWithRedirect();
+  };
+
   return (
     <div className="relative flex flex-col lg:flex-row justify-center min-h-screen overflow-hidden">
-    
       <div className="lg:w-1/2 w-full p-6 m-auto bg-white rounded-md shadow-xl">
         <h1 className="text-3xl font-semibold text-center text-green-700 uppercase">
           Welcome to Muzadde Today
@@ -73,7 +80,7 @@ function Login() {
           </div>
           <div className="mt-6">
             <button
-              type="button" 
+              type="button"
               onClick={handleLogin}
               className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-pink-600 focus:outline-none focus:bg-purple-600"
             >
@@ -81,24 +88,25 @@ function Login() {
             </button>
           </div>
         </form>
-        <div className="relative flex items-center justify-center w-full mt-6 border border-t">
-          <div className="absolute px-5 bg-white">
-            <p className="text-sm text-gray-800">Or</p>
+        <div className="mt-4">
+          <div className="flex items-center justify-center text-gray-800 mb-4">
+            <hr className="flex-grow border-t" />
+            <p className="mx-4">Or</p>
+            <hr className="flex-grow border-t" />
           </div>
-        </div>
-        <div className="flex mt-4 justify-center">
           <button
-            className="flex items-center justify-center w-full p-2 border border-gray-600 rounded-md focus:ring-2 focus:ring-offset-1 focus:ring-violet-600"
-            onClick={() => console.log("Continue with Google clicked")}
+            type="button"
+            onClick={handleLoginWithAuth0}
+            className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-blue-500 focus:outline-none focus:bg-blue-500 flex items-center justify-center"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 32 32"
-              className="w-5 h-5 fill-current text-blue-600"
+              className="w-5 h-5 fill-current text-blue-600 mr-2"
             >
               <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
             </svg>
-            <span className="ml-2">Continue with Google</span>
+            Login with Google
           </button>
         </div>
         <p className="mt-8 text-xs font-light text-center text-gray-700">
@@ -108,8 +116,6 @@ function Login() {
           </Link>
         </p>
       </div>
-
-      
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <Auth0Provider
+    domain="dev-h33vh3ddi3fzu543.us.auth0.com"
+    clientId="eM1czitHknpMztnyra4fXzDQBCsjnOhB"
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>,
+);
